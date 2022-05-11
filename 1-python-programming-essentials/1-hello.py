1200309595cabbf1630fec9343e7b0278626e617
Sherry <105389513+sherryc101@users.noreply.github.com>
2022-05-11 16:00:42 -0500

    Update 1-hello.py

--- 1-python-programming-essentials/1-hello.py
+++ 1-python-programming-essentials/1-hello.py
@@ -1,21 +1,22 @@
 """
-document string
-explain the purpose of this file
-"""
+1 - docstring and comment
+use """ """ to docstring -document string - to explain the purpose of this file
+use # # to comment out short sentence in one line - explain the following code
 
-# comment
-# explain the following code
-
-print("hello, world!")
-
-"""
+2 - tools
 what is an IDE - integrated development environment - software application to write, save and run programs
 controls - code pane - console
 1. CodeSkulptor3 - py3.codeskulptor.org
 2. Thonny - install Thonny
 3. Atom - install python, then atom
-try below
 """
+
+"""
+print hello as your first python code
+"""
+print("hello, world!")
+
+# have a try below
 
 import simplegui
 message = "Welcome!"
